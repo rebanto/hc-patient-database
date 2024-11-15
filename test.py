93b94ca6daf4a146c63ff3fefb4ff6f43a5d3a7c
Rebanto <rebanto.nath@gmail.com>
2024-11-15 11:24:51 -0500

    test commit

---
+++ test.py
@@ -0,0 +1,12 @@
+import libsql_client
+
+def deletePerson(name):
+    with libsql_client.create_client_sync(
+    "libsql://patientdata-heyingz.turso.io",
+    auth_token="eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9.eyJhIjoicnciLCJpYXQiOjE3MzEyNTY3NzEsImlkIjoiMzg0YTcxYzQtODA3OS00NDYyLWI3MzktZDNmZWUxOGU4MzAzIn0.Dy6ULddDvLPfHKi77E9L85IfAZ-svnGynCXLQRlDuH4hSABwT7ixlNYfxE02jn35yHp3L1FZC9D4L64KpxpiCg"
+
+    ) as client:
+        command = "DELETE FROM Patient WHERE first_name = \"" + name +"\""
+        result = client.execute(command)
+
+deletePerson("Elizabeth")
