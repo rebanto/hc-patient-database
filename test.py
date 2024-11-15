62a4b0d2b54b1d0f2dd0603786348cea3d3b81a8
2000162 <145173518+2000162@users.noreply.github.com>
2024-11-15 12:10:36 -0500

    Updated new.py

--- test.py
+++ test.py
@@ -8,10 +8,12 @@ def deletePerson(name):
         command = "DELETE FROM Patient WHERE first_name = \"" + name +"\""
         result = client.execute(command)
 
-def addPerson(name):
+def addPerson(id, fname, lname, dob):
     with libsql_client.create_client_sync(
         "libsql://patientdata-heyingz.turso.io",
         auth_token="eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9.eyJhIjoicnciLCJpYXQiOjE3MzEyNTY3NzEsImlkIjoiMzg0YTcxYzQtODA3OS00NDYyLWI3MzktZDNmZWUxOGU4MzAzIn0.Dy6ULddDvLPfHKi77E9L85IfAZ-svnGynCXLQRlDuH4hSABwT7ixlNYfxE02jn35yHp3L1FZC9D4L64KpxpiCg"
     ) as client:
-        command = "INSERT INTO patietdata (patient_id, first_name, last_name, DOB) VALUES (1234, 'Pranav', 'Sreepada', 'December 4, 2009');"
+        command = "INSERT INTO patient_data (ID, first_name, last_name, DOB) VALUES (\"" + str(id) + "\", \"" + fname + "\", \"" + lname + "\", \"" + dob + "\");"
         result = client.execute(command)
+
+addPerson(1234, "Pranav", "Sreepada", "December 4, 2009")
