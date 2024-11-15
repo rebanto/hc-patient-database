a45b851c754d6ea4a83c8d51cbbccd8f243c4817
2000162 <145173518+2000162@users.noreply.github.com>
2024-11-15 11:33:17 -0500

    base

--- test.py
+++ test.py
@@ -2,11 +2,16 @@ import libsql_client
 
 def deletePerson(name):
     with libsql_client.create_client_sync(
-    "libsql://patientdata-heyingz.turso.io",
-    auth_token="eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9.eyJhIjoicnciLCJpYXQiOjE3MzEyNTY3NzEsImlkIjoiMzg0YTcxYzQtODA3OS00NDYyLWI3MzktZDNmZWUxOGU4MzAzIn0.Dy6ULddDvLPfHKi77E9L85IfAZ-svnGynCXLQRlDuH4hSABwT7ixlNYfxE02jn35yHp3L1FZC9D4L64KpxpiCg"
-
+        "libsql://patientdata-heyingz.turso.io",
+        auth_token="eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9.eyJhIjoicnciLCJpYXQiOjE3MzEyNTY3NzEsImlkIjoiMzg0YTcxYzQtODA3OS00NDYyLWI3MzktZDNmZWUxOGU4MzAzIn0.Dy6ULddDvLPfHKi77E9L85IfAZ-svnGynCXLQRlDuH4hSABwT7ixlNYfxE02jn35yHp3L1FZC9D4L64KpxpiCg"
     ) as client:
         command = "DELETE FROM Patient WHERE first_name = \"" + name +"\""
         result = client.execute(command)
 
-deletePerson("Elizabeth")
+def addPerson(name):
+    with libsql_client.create_client_sync(
+        "libsql://patientdata-heyingz.turso.io",
+        auth_token="eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9.eyJhIjoicnciLCJpYXQiOjE3MzEyNTY3NzEsImlkIjoiMzg0YTcxYzQtODA3OS00NDYyLWI3MzktZDNmZWUxOGU4MzAzIn0.Dy6ULddDvLPfHKi77E9L85IfAZ-svnGynCXLQRlDuH4hSABwT7ixlNYfxE02jn35yHp3L1FZC9D4L64KpxpiCg"
+    ) as client:
+        command = "INSERT INTO patietdata (patient_id, first_name, last_name, )"
+        result = client.execute(command)
