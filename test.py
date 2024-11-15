ae93c4d804ef1f859900963936089c4ec9d6b2a3
CoachHeyingFCS <heyingz@fultonschools.org>
2024-11-15 15:02:22 -0500

    Update test.py
    This has the update function in it.

--- test.py
+++ test.py
@@ -11,6 +11,18 @@ def deletePerson(name):
     ) as client:
         command = "DELETE FROM Patient WHERE first_name = \"" + name +"\""
         result = client.execute(command)
+def delete(table, column, value):
+    with libsql_client.create_client_sync(
+    "libsql://patientdata-heyingz.turso.io",
+    auth_token="eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9.eyJhIjoicnciLCJpYXQiOjE3MzEyNTY3NzEsImlkIjoiMzg0YTcxYzQtODA3OS00NDYyLWI3MzktZDNmZWUxOGU4MzAzIn0.Dy6ULddDvLPfHKi77E9L85IfAZ-svnGynCXLQRlDuH4hSABwT7ixlNYfxE02jn35yHp3L1FZC9D4L64KpxpiCg"
+    ) as client:
+        command = ""
+        if isinstance(value, int):
+            command = "DELETE FROM "+ table +" WHERE "+column +"=" + str(value) + ";"
+        else:
+            command = "DELETE FROM "+ table +" WHERE "+column +"=" + stringEscape(value) + ";"
+        print(command)
+        result = client.execute(command)
 
 def addPerson(id, fname, lname, dob):
     with libsql_client.create_client_sync(
@@ -48,7 +60,11 @@ def addBed(id, bedNum, start):
         command = "INSERT INTO room(ID, bed_number, start_time, end_time) VALUES ("+ str(id) +","+ str(bedNum) +" , " + stringEscape(start) +", " + stringEscape("x")+");"
         result = client.execute(command)
 
+def delete(table, column, value):
+    with libsql_client.create_client_sync(
+        "libsql://patientdata-heyingz.turso.io",
+        auth_token="eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9.eyJhIjoicnciLCJpYXQiOjE3MzEyNTY3NzEsImlkIjoiMzg0YTcxYzQtODA3OS00NDYyLWI3MzktZDNmZWUxOGU4MzAzIn0.Dy6ULddDvLPfHKi77E9L85IfAZ-svnGynCXLQRlDuH4hSABwT7ixlNYfxE02jn35yHp3L1FZC9D4L64KpxpiCg"
+    ) as client:
+        command = "DELETE FROM Patient WHERE first_name = \"" + name +"\""
+        result = client.execute(command)
 
-
-
-addPerson(1234, "Pranav", "Sreepada", "December 4, 2009")
